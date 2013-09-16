6882d93236668159d86e35b20a914648193a428f
SymbolixDEV <shabi_bjk@abv.bg>
2013-09-16 16:23:04 +0300

    Update showoptions.cmake

--- cmake/showoptions.cmake
+++ cmake/showoptions.cmake
@@ -1,6 +1,6 @@
 # output generic information about the core and buildtype chosen
 
-message("* Shadowhack 3 revision  : ${rev_id_str}")
+message("* TrinityCore revision  : ${rev_id_str}")
 if( UNIX )
   message("* Build binaries in      : ${CMAKE_BUILD_TYPE} mode")
 endif()
